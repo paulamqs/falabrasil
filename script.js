925b92ae4a11dd0a53570f111e63d34d3298472c
paulamqs <106463129+paulamqs@users.noreply.github.com>
2022-05-29 01:12:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+ 
+      let popup = document.getElementById("popup");
+      function abrirPopup() {
+        popup.classList.add("openpopup");
+      }
+
+      function cerrarPopup() {
+        popup.classList.remove("openpopup");
+      }
